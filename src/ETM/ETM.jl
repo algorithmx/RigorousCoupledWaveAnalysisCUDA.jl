515c53af3694c4af7fe56750b87a67d5ddc0092c
Yunlong Lian <algorithm.lian@gmail.com>
2022-07-25 12:26:38 +0800

    modify ETM, add GPU code

--- src/ETM/ETM.jl
+++ src/ETM/ETM.jl
@@ -1,22 +1,22 @@
-#Enhanced transmission matrix algorithm by Moharam et al.
+# Enhanced transmission matrix algorithm by Moharam et al.
 module ETM
 using LinearAlgebra
+using SparseArrays
+using CUDA
 using ..Common
-export etm_reftra,etm_amplitudes,etm_propagate,etm_flow,etm_reftra_flows
+export etm_reftra, etm_amplitudes, etm_propagate,etm_flow, etm_reftra_flows
 #utility method for dealing with eigenmodes
-function F(em)
-    return Matrix([em.W em.W;em.V -em.V])
-end
+@inline F(em) = [em.W   em.W;  em.V   -em.V]
+@inline showcumat(A,name="") = println("name = $(name) , size = $(A.dims) , offset = $(A.offset) , maxsize = $(A.maxsize)\nstorage = $(A.storage)")
 """
-    etm_propagate(sup,sub,em,ψin,grd)
-    etm_propagate(sup,sub,em,ψin,grd,get_r)
+    etm_propagate(sup,sub,em,ψin)
+    etm_propagate(sup,sub,em,ψin,get_r)
 The propagation of waves according to the ETM method
 # Arguments
 * `sup` :  superstrate halfspace object
 * `sub` :  substrate halfspace object
 * `em` :  array with eigenmodes for all layers
 * `ψin` :  incoming (source) amplitude vector
-* `grd` : RCWA grid object
 * `get_r` : set false if you do not need the internal backward propagating waves (only required for absorption calculation)
 # Outputs
 * `ψref` : reflected amplitude vector
@@ -24,32 +24,139 @@ The propagation of waves according to the ETM method
 * `ψp` : array of internal back propagating wave vectors in each layer
 * `ψm` : array of internal forward propagating wave vectors in each layer
 """
-function etm_propagate(sup,sub,em,ψin,grd,get_r=true)
-    ψm=Array{Array{Complex{Float64},1},1}(undef,length(em)) #preallocate
-    ψp=Array{Array{Complex{Float64},1},1}(undef,length(em)) 
-if length(em)>0
-    #backward iteration
-    a=Array{Array{Complex{Float64},2},1}(undef,length(em)) #preallocate
-    b=Array{Array{Complex{Float64},2},1}(undef,length(em))
-    a[end],b[end]=slicehalf(F(em[end])\Matrix([I;-sub.V])) #transmission matrix for a wave in the last layer into the substrate
-    for cnt=length(em):-1:2
-        a[cnt-1],b[cnt-1]=slicehalf(F(em[cnt-1])\F(em[cnt])*[em[cnt].X*(a[cnt]/b[cnt])*em[cnt].X ;I]) #successively compute the transmission matrix from the i-th layer into the substrate
+function etm_propagate(
+    sup::Halfspace,
+    sub::Halfspace,
+    em::Vector{Eigenmodes}, 
+    sp_ψin::SparseVector,
+    get_r::Bool=true
+    )
+    etm_propagate(sup, sub, em, Vector(sp_ψin), get_r)
+end
+function etm_propagate(
+    sup::Halfspace,
+    sub::Halfspace,
+    em::Vector{Eigenmodes}, 
+    ψin::Vector,
+    get_r::Bool=true
+    )
+    @assert ! (sup.V isa CuArray)
+    @assert ! (sub.V isa CuArray)
+    @assert all([!(emi.X isa CuArray) for emi in em])
+
+    T = eltype(ψin)
+
+    ψm = Array{Array{T,1},1}(undef,length(em))
+    ψp = Array{Array{T,1},1}(undef,length(em))
+    if length(em)>0
+        # 1. [backward iteration]
+        a = Array{Array{T,2},1}(undef,length(em)) 
+        b = Array{Array{T,2},1}(undef,length(em))
+        # transmission matrix for a wave in the last layer into the substrate
+        a[end],b[end] = slicehalf(F(em[end])\Matrix([I;-sub.V])) 
+        for cnt = length(em):-1:2  # [backward iteration]
+            # successively compute the transmission matrix from the i-th layer into the substrate
+            a[cnt-1],b[cnt-1] = slicehalf(F(em[cnt-1])\F(em[cnt])*[em[cnt].X*(a[cnt]/b[cnt])*em[cnt].X ; I])
+        end
+        # 2. [forward iteration]
+        # compute the reflected wave and the forward wave in the first layer
+        # bottleneck
+        ψref,ψm1 = slicehalf( -cat([I;sup.V],F(em[1])*[em[1].X*(a[1]/b[1])*em[1].X;I],dims=2) \ ([I;-sup.V]*ψin) )
+        ψm[1] = vec(ψm1) 
+        for cnt = 1:length(em)-1  # [forward iteration]
+            # successively compute the forward wave in the i-th layer
+            ψm[cnt+1] = b[cnt]\I*(em[cnt].X*ψm[cnt]) 
+            # if required, compute the backward wave in the i-th layer
+            get_r && (ψp[cnt] = em[cnt].X*a[cnt]*ψm[cnt+1]) 
+        end
+        # if required, computethe backward wave in the first layer
+        get_r && (ψp[1] = em[1].X*(a[1]/b[1])*em[1].X*ψm[1]) 
+        # compute the transmitted wave from the forward wave in the last layer
+        ψtra = b[end]\I*em[end].X*ψm[end] 
+        # compute the backward wave in the last layer, if required
+        get_r && (ψp[end] = em[end].X*a[end]*ψtra) 
+    else 
+        # the case for "empty" model (single interface, no layers)
+        ψref,ψtra = slicehalf([-I I;sup.V sub.V]\[I*ψin;sup.V*ψin])
     end
-	#forward iteration
-    ψref,ψm1=slicehalf(-cat([I;sup.V],F(em[1])*[em[1].X*(a[1]/b[1])*em[1].X;I],dims=2)\([I;-sup.V]*ψin)) #compute the reflected wave and the forward wave in the first layer
-    ψm[1]=vec(ψm1) 
-    for cnt=1:length(em)-1
-        ψm[cnt+1]=b[cnt]\I*(em[cnt].X*ψm[cnt]) #successively compute the forward wave in the i-th layer
-		get_r&&(ψp[cnt]=em[cnt].X*a[cnt]*ψm[cnt+1]) #if required, compute the backward wave in the i-th layer
-    end
-	get_r&&(ψp[1]=em[1].X*(a[1]/b[1])*em[1].X*ψm[1]) #if required, computethe backward wave in the first layer
-    ψtra=b[end]\I*em[end].X*ψm[end] #compute the transmitted wave from the forward wave in the last layer
-	get_r&&(ψp[end]=em[end].X*a[end]*ψtra) #compute the backward wave in the last layer, if required
-	else
-	ψref,ψtra=slicehalf([-I I;sup.V sub.V]\[I*ψin;sup.V*ψin]) #the case for "empty" model (single interface, no layers)
-	end
+    #TODO normalize this:  size of ψref, ψtra are not the same but equivalent   
     return ψref,ψtra,ψp,ψm
 end
+function etm_propagate(
+    sup::Halfspace,
+    sub::Halfspace,
+    ems_gpu::Vector{Eigenmodes}, 
+    cu_ψin::CuArray,
+    get_r=true
+    )
+    @assert sup.V isa CuArray
+    @assert sub.V isa CuArray
+    @assert all([(emi.X isa CuArray) for emi in ems_gpu])
+
+    T = eltype(cu_ψin)
+
+    dim_block = size(sup.V,1)
+    @assert dim_block==size(sup.V,2)==size(sub.V,1)==size(sub.V,2)==length(cu_ψin)
+    @assert all(size(em.X)==(dim_block,dim_block) for em in ems_gpu)
+
+    cuI   = CuArray(Diagonal{T}(I,dim_block))
+
+    if length(ems_gpu)==0
+        CUDA.synchronize()
+        # the case for "empty" model (single interface, no layers)
+        ψref, ψtra = slicehalf([-cuI  cuI; sup.V  sub.V] \ [cu_ψin; sup.V * cu_ψin])
+    else
+        # temporary vector
+        _b = [-cu_ψin; sup.V*cu_ψin]
+        CUDA.synchronize()
+
+        # 0> [initialize]
+        ab = Vector{CuArray{T,2}}(undef,length(ems_gpu))
+        # transmission matrix for a wave in the last layer into the substrate
+        ab[end] = F(ems_gpu[end]) \ vcat(cuI,-sub.V)
+
+        # 1> [backward iteration]
+        for cnt = length(ems_gpu):-1:2  # [backward iteration]
+            # successively compute the transmission matrix from the i-th layer into the substrate
+            ab[cnt-1] = F(ems_gpu[cnt-1]) \ (F(ems_gpu[cnt])*[ems_gpu[cnt].X*a_over_b(ab[cnt])*ems_gpu[cnt].X ; cuI])
+        end
+
+        # 2> [intermediate step]
+        # compute the reflected wave and the forward wave in the first layer
+        # CUDA version : Array(CuArray(A) \ CuArray(b))
+        # bottleneck #2, optimized
+        # TODO align the vector [-cu_ψin; v_tmp] and the vector [cu_ψin; v_tmp]
+        # in the length(ems_gpu)==0 such that the minus sign agrees
+        tmp = F(ems_gpu[1])*vcat(ems_gpu[1].X*a_over_b(ab[1])*ems_gpu[1].X, cuI)
+        ψref, ψm1 = slicehalf(( cat(vcat(cuI,sup.V), tmp, dims=2) \ _b ))
+
+        # containers
+        ψm = Vector{CuVector{Complex{Float64}}}(undef,length(ems_gpu)) 
+        ψp = Vector{CuVector{Complex{Float64}}}(undef,length(ems_gpu))
+        ψm[1] = vec(ψm1) # preserves location
+
+        # 3> [forward iteration]
+        for cnt = 1:length(ems_gpu)-1  # [forward iteration]
+            # successively compute the forward wave in the i-th layer
+            ψm[cnt+1] = take_b(ab[cnt]) \ (ems_gpu[cnt].X*ψm[cnt])
+            # if required, compute the backward wave in the i-th layer
+            get_r && (ψp[cnt] = ems_gpu[cnt].X*take_a(ab[cnt])*ψm[cnt+1])
+        end
+        # if required, computethe backward wave in the first layer
+        get_r && (ψp[1] = ems_gpu[1].X*a_over_b(ab[1])*ems_gpu[1].X*ψm[1])
+        # compute the transmitted wave from the forward wave in the last layer
+        ψtra = take_b(ab[end]) \ (ems_gpu[end].X*ψm[end])
+        # compute the backward wave in the last layer, if required
+        get_r && (ψp[end] = ems_gpu[end].X*take_a(ab[end])*ψtra)
+
+    end
+
+    CUDA.synchronize()
+
+    #TODO normalize this:  size of ψref, ψtra are not the same but equivalent
+    return ψref,ψtra,ψp,ψm
+end
+
 """
     etm_reftra(ψin,m,grd,λ)
     etm_reftra(ψin,m,grd,λ,em,sup,sub)
@@ -67,19 +174,23 @@ lation)
 * `R` : reflection by the device 
 * `T` : transmission by the device 
 """
-function etm_reftra(ψin,m::RCWAModel,grd::RCWAGrid,λ,em,sup,sub)
-	kzin=grd.k0[3]#*real(sqrt(get_permittivity(m.εsup,λ)))
-	ro,to,r,t=etm_propagate(sup,sub,em,ψin,grd,false) #propagate amplitudes
-    R=a2p(0ro,ro,sup.V,I,kzin) #compute amplitudes to powers
-    T=-a2p(to,0to,sub.V,I,kzin)
+function etm_reftra(ψin,grd::RCWAGrid,λ,ems,sup,sub)
+    kzin=grd.k0[3] # * real(sqrt(get_permittivity(m.εsup,λ)))
+    # propagate amplitudes
+    ro,to,r,t = etm_propagate(sup,sub,ems,ψin,false)
+    R =  a2p(0ro,ro,sup.V,I,kzin) # compute amplitudes to powers
+    T = -a2p(to,0to,sub.V,I,kzin)
     return R,T
 end
-function etm_reftra(s,m::RCWAModel,grd::RCWAGrid,λ)
-	ems=eigenmodes(grd,λ,m.layers) #eigenmodes of all layers
-	ref=halfspace(grd.Kx,grd.Ky,m.εsup,λ) #superstrate and substrate
-	tra=halfspace(grd.Kx,grd.Ky,m.εsub,λ)
-	R,T=etm_reftra(s,m,grd,λ,ems,ref,tra)
-	return R,T
+function etm_reftra(ψin,m::RCWAModel,grd::RCWAGrid,λ)
+    # println("etm_reftra():")
+    CUDA.@time ems = eigenmodes(grd,λ,m.layers)
+    ug = (grd.V0 isa CuArray)
+    tra = halfspace(grd.Kx,grd.Ky,m.εsub,λ;use_gpu=ug)
+    ref = halfspace(grd.Kx,grd.Ky,m.εsup,λ;use_gpu=ug)
+    CUDA.@time R, T = etm_reftra(ψin,grd,λ,ems,ref,tra)
+    # println("---")
+    return R, T
 end
 """
     etm_reftra_flows(ψin,m,grd,λ)
@@ -100,19 +211,23 @@ lation)
 * `flw` : array containing the power flows between layers 
 """
 function etm_reftra_flows(s,m::RCWAModel,grd::RCWAGrid,λ,ems,sup,sub)
-	kzin=grd.k0[3]#*real(sqrt(get_permittivity(m.εsup,λ)))
-    ro,to,b,a=etm_propagate(sup,sub,ems,s,grd) #propagate waves
-    R=a2p(0ro,ro,sup.V,I,kzin) #reflected and transmitted power
-    T=-a2p(to,0to,sub.V,I,kzin)
-	flw=[etm_flow(a[i],b[i],ems[i],kzin) for i=1:length(a)] #intermediate power flows
+    kzin=grd.k0[3]#*real(sqrt(get_permittivity(m.εsup,λ)))
+    ro,to,b,a=etm_propagate(sup,sub,ems,s) #propagate waves
+    R =  a2p(0ro,ro,sup.V,I,kzin) # reflected power
+    T = -a2p(to,0to,sub.V,I,kzin) # transmitted power
+    #intermediate power flows 
+    flw=[etm_flow(a[i],b[i],ems[i],kzin) for i=1:length(a)]
     return R,T,flw
 end
 function etm_reftra_flows(s,m::RCWAModel,grd::RCWAGrid,λ)
-	ems=eigenmodes(grd,λ,m.layers) # layer eigenmodes
-	ref=halfspace(grd.Kx,grd.Ky,m.εsup,λ) #superstrate and substrate eigenmodes
-	tra=halfspace(grd.Kx,grd.Ky,m.εsub,λ)
-	R,T,flw=etm_reftra_flows(s,m,grd,λ,ems,ref,tra)
-	return R,T,flw
+    # println("etm_reftra_flows()")
+    CUDA.@time  ems=eigenmodes(grd,λ,m.layers)
+    ug = (grd.V0 isa CuArray)
+    ref=halfspace(grd.Kx,grd.Ky,m.εsup,λ;use_gpu=ug)
+    tra=halfspace(grd.Kx,grd.Ky,m.εsub,λ;use_gpu=ug)
+    CUDA.@time  R,T,flw=etm_reftra_flows(s,m,grd,λ,ems,ref,tra)
+    # println("---")
+    return R,T,flw
 end
 """
     etm_amplitudes(ψin,m,grd,λ)
@@ -121,8 +236,8 @@ Computes the amplitude vectors of forward and backward propagating waves through
 # Arguments
 * `ψin` :  incoming (source) amplitude vector
 * `m` :  RCWA model object
-* `grd` : RCWA grid object
 * `λ` : free-space wavelength
+* `grd` : RCWA grid
 * `em` :  array with eigenmodes for all layers (computed from m if not given)
 * `sup` :  superstrate halfspace object (computed from m if not given)
 * `sub` :  substrate halfspace object (computed from m if not given)  
@@ -131,16 +246,19 @@ lation)
 * `a` : amplitude vectors of forward waves
 * `b` : amplitude vectors of backward waves
 """
-function etm_amplitudes(ψin,m::RCWAModel,grd::RCWAGrid,λ,em,sup,sub)
-    ro,to,r,t=etm_propagate(sup,sup,em,ψin,grd) #propagate wave
-	return cat([ψin],t,[to],dims=1),cat([ro],r,[0ro],dims=1) #put in order
+function etm_amplitudes(ψin,m::RCWAModel,grd::RCWAGrid,λ::Real,em,sup,sub)
+    ro,to,r,t=etm_propagate(sup,sup,em,ψin) #propagate wave
+    return cat([ψin],t,[to],dims=1),cat([ro],r,[0ro],dims=1) #put in order
 end	
-function etm_amplitudes(ψin,m::RCWAModel,grd::RCWAGrid,λ)
-	ems=eigenmodes(grd,λ,m.layers) 	#layer eigenmodes
-	ref=halfspace(grd.Kx,grd.Ky,m.εsup,λ) #superstrate
-	tra=halfspace(grd.Kx,grd.Ky,m.εsub,λ) #substrate
-	a,b=etm_amplitudes(ψin,m,grd,λ,ems,ref,tra)
-	return a,b
+function etm_amplitudes(ψin,m::RCWAModel,grd::RCWAGrid,λ::Real)
+    # println("etm_amplitudes()")
+    CUDA.@time ems = eigenmodes(grd,λ,m.layers) 	#layer eigenmodes
+    ug = (grd.V0 isa CuArray)
+    ref=halfspace(grd.Kx,grd.Ky,m.εsup,λ;use_gpu=ug) #superstrate
+    tra=halfspace(grd.Kx,grd.Ky,m.εsub,λ;use_gpu=ug) #substrate
+    CUDA.@time a,b = etm_amplitudes(ψin,m,grd,λ,ems,ref,tra)
+    # println("---")
+    return a,b
 end
 """
     etm_flow(a,b,em,kz0)
@@ -153,10 +271,11 @@ Computes the power flow in z direction at a single location
 # Outputs
 * `flow` : power flow in the z direction
 """
-function etm_flow(a,b,em,kz0)
-	ex,ey=a2e2d(a+b,em.W) #electric field
-	hx,hy=a2e2d(a-b,em.V) #magnetic field
-	return imag(sum(ex.*conj.(hy)-ey.*conj.(hx)))/kz0 #poynting vector z coordinate
+function etm_flow(a, b, em, kz0)
+    ex, ey = a2e2d(a+b, em.W)  # electric field
+    hx, hy = a2e2d(a-b, em.V)  # magnetic field
+    # poynting vector z coordinate
+    return imag(sum(ex.*conj.(hy)-ey.*conj.(hx)))/kz0 
 end
 
 end
